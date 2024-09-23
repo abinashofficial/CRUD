f1d2a3bc7537f0a4bd18f24a54991ee8440a9920
abinashofficial <abinash1411999@gmail.com>
2024-09-23 17:30:00 +0530

    rer

--- app/routes.go
+++ app/routes.go
@@ -29,55 +29,12 @@ func runServer(envPort string, h handlers.Store) {
 	r.HandleFunc("/public/recovery", h.FieldsHandler.PasswordChange).Methods(http.MethodPut)
 
 
-corsMiddleware := handler.CORS(
-
-	handler.AllowedOrigins([]string{"*"}), // Allowing all origin as of now
-
-	handler.AllowedHeaders([]string{
-		"Accept",
-		"Content-Type",
-		"contentType",
-		"Content-Length",
-		"Accept-Encoding",
-		"Client-Security-Token",
-		"X-CSRF-Token",
-		"X-Auth-Token",
-		"processData",
-		"Authorization",
-		"Access-Control-Request-Headers",
-		"Access-Control-Request-Method",
-		"Connection",
-		"Host",
-		"Origin",
-		"User-Agent",
-		"Referer",
-		"Cache-Control",
-		"X-header",
-		"X-Requested-With",
-		"timezone",
-		"locale",
-		"email",
-		"tenant",
-		"dealer",
-		"tap-api-token",
-		"gzip-compress",
-		"task",
-		"x-tap-accesskey",
-		"x-tap-secretkey",
-		"access_token",
-		"application",
-	}),
-
-	handler.AllowedMethods([]string{
-		"POST",
-		"GET",
-		"DELETE",
-		"PUT",
-		"PATCH",
-		"OPTIONS"}),
-
-		handler.AllowCredentials(),
-	)
+    // Allow all origins with the CORS middleware
+    corsMiddleware := handler.CORS(
+        handler.AllowedOrigins([]string{"*"}), // Allow all origins
+        handler.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}), // Allowed methods
+        handler.AllowedHeaders([]string{"Content-Type", "Authorization"}), // Allowed headers
+    )
  	fmt.Printf("Server listening on port %d...\n", envPort)
 
     log.Fatal(http.ListenAndServe(":8080", corsMiddleware(r)))
