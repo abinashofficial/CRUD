72e59559ebd8037a9a6098cd57c059e5c409dfb0
abinashofficial <abinash1411999@gmail.com>
2024-09-24 02:25:44 +0530

    verify mob

--- handlers/fields/store.go
+++ handlers/fields/store.go
@@ -151,7 +151,7 @@ func (h fieldHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 
 
 func (h fieldHandler) Login(w http.ResponseWriter, r *http.Request) {
-	req := model.Login{}
+	req := model.Signup{}
 	// ctx := r.Context()
 
 	err := json.NewDecoder(r.Body).Decode(&req)
@@ -159,9 +159,9 @@ func (h fieldHandler) Login(w http.ResponseWriter, r *http.Request) {
 		utils.ErrorResponse(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	query := "SELECT email, password FROM employees WHERE email = $1"
+	query := "SELECT employee_id, first_name, last_name,mobile_number,  email, date_of_birth,gender,  password FROM employees WHERE email = $1"
 	password:= ""
-    err = h.sqlDB.QueryRow(query, req.Email).Scan(&req.Email, &password)
+    err = h.sqlDB.QueryRow(query, req.Email).Scan(&req.EmployeeID,&req.FirstName,&req.LastName,&req.MobileNumber, &req.Email,&req.DateOfBirth,&req.Gender, &password)
 	if err != nil {
 		utils.ErrorResponse(w, "Invalid Email", http.StatusInternalServerError)
 	}else if password != req.Password{
@@ -182,14 +182,22 @@ func (h fieldHandler) Signup(w http.ResponseWriter, r *http.Request) {
 	}
 	query := "SELECT email FROM employees WHERE email = $1"
     err = h.sqlDB.QueryRow(query, req.Email).Scan(&req.Email)
-	if err != nil {
-		sqlStatement := `INSERT INTO employees (first_name, last_name, mobile_number, email, date_of_birth, gender, password ) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING employee_id`
-		err = h.sqlDB.QueryRow(sqlStatement, req.FirstName, req.LastName,req.MobileNumber, req.Email, req.DateOfBirth, req.Gender, req.Password).Scan(&req.EmployeeID)
+		if err == nil {
+			utils.ErrorResponse(w, "Email ID Already Exist", http.StatusInternalServerError)
+			return
+		}
+		query = "SELECT mobile_number FROM employees WHERE mobile_number = $1"
+		err = h.sqlDB.QueryRow(query, req.MobileNumber).Scan(&req.MobileNumber)
+			if err == nil {
+				utils.ErrorResponse(w, "Mobile Number Already Exist", http.StatusInternalServerError)
+				return
+			}
+
+			query = `INSERT INTO employees (first_name, last_name, mobile_number, email, date_of_birth, gender, password ) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING employee_id`
+		err = h.sqlDB.QueryRow(query, req.FirstName, req.LastName,req.MobileNumber, req.Email, req.DateOfBirth, req.Gender, req.Password).Scan(&req.EmployeeID)
 		if err != nil {
 			utils.ErrorResponse(w, err.Error(), http.StatusInternalServerError)
-		}
-		}else{
-			utils.ErrorResponse(w, "Email ID Already Exist", http.StatusInternalServerError)
+			return
 		}
 
 
